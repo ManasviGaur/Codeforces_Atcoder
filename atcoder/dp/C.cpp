06e18548864d341640e376e811110e2ce26cdcc8
Manasvi Gaur <manasvigaur619@gmail.com>
2021-07-29 15:08:00 +0530

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/24609227

---
+++ atcoder/dp/C.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ld long double
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define vt vector
+#define pb push_back
+#define F first
+#define S second
+#define u_s unordered_set
+#define u_m unordered_map
+#define p_q priority_queue
+
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifndef ONLINE_JUDGE
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define deb(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+
+int __pow(int x, int y, int m) {int res = 1;while(y>0){if(y&1) res = (res*x)%m; y>>=1; x = (x*x)%m;}return res%m;}
+int __pow(int x, int y) {int res = 1;while(y>0){if(y&1) res = (res*x); y>>=1; x = (x*x);}return res;}
+int gcd(int a, int b) {if(b==0) return a; return gcd(b, a % b);}
+int mi(int x, int M) { return __pow(x,M-2, M);}
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
+
+const int N = 1e5, inf = 1e17, M = 1e9 + 7;
+int test,n,x,y,m,k,query;
+
+void solve(){
+    cin>>n;
+    vt<int> a(n+1, 0), b(n+1, 0), c(n+1, 0);
+    for(int i=1; i<=n; i++){
+        cin>>a[i]>>b[i]>>c[i];
+    }
+    vt<vt<int>> dp(n+1, vt<int> (3,0));
+    debug()<<deb(dp);
+    for(int i=1; i<=n; i++){
+        dp[i][0] = max(dp[i-1][1] + b[i], dp[i-1][2] + c[i]);
+        dp[i][1] = max(dp[i-1][0] + a[i], dp[i-1][2] + c[i]);
+        dp[i][2] = max(dp[i-1][0] + a[i], dp[i-1][1] + b[i]);
+    }
+    debug()<<deb(dp);
+    cout<<max(dp[n][0], max(dp[n][1], dp[n][2]))<<endl;
+
+}
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    test=1;
+    //cin>>test;
+    while(test--){
+        solve();
+    }
+    #ifndef ONLINE_JUDGE
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s." << endl;
+    #endif
+    return 0;
+}
