b1f8770b39c4bc5adf3a8819e3792b045e1f648a
Manasvi Gaur <manasvigaur619@gmail.com>
2021-07-29 14:44:00 +0530

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/24608920

--- atcoder/dp/A.cpp
+++ atcoder/dp/A.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-/********************************MACROS********************************/
- 
+
 #define int long long
 #define ld long double
 #define all(x) (x).begin(), (x).end()
@@ -17,9 +15,8 @@ using namespace std;
 #define u_s unordered_set
 #define u_m unordered_map
 #define p_q priority_queue
- 
-/*******************************DEBUGGER*******************************/
- 
+
+
 #define sim template < class c
 #define ris return * this
 #define dor > debug & operator <<
@@ -48,55 +45,41 @@ sim dor(const c&) { ris; }
 #endif
 };
 #define deb(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
- 
-/******************************ALGORITHMS******************************/
- 
+
+
 int __pow(int x, int y, int m) {int res = 1;while(y>0){if(y&1) res = (res*x)%m; y>>=1; x = (x*x)%m;}return res%m;}
 int __pow(int x, int y) {int res = 1;while(y>0){if(y&1) res = (res*x); y>>=1; x = (x*x);}return res;}
 int gcd(int a, int b) {if(b==0) return a; return gcd(b, a % b);}
 int mi(int x, int M) { return __pow(x,M-2, M);}
 bool sortbysec(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
-/******************************MAIN**CODE******************************/
- 
-const int N = 1e5+2, inf = 1e18, M = 1e9+7;
-int test, n, k, m, x, query;
 
-string func(int n, int len){
-    string res(len, '0');
-    int i = len - 1;
-    while(n && len >= 0){
-        int x = n%2;
-        if(x){
-            res[i] = '1';
-        }
-        i--;
-        n /= 2;
+const int N = 1e5, inf = 1e17, M = 1e9 + 7;
+int test,n,x,y,m,k,query;
+
+void solve(){
+    cin>>n;
+    vt<int> a(n);
+    for(auto &x:a) cin>>x;
+    vt<int> dp(n+1, 0);
+    dp[1] = 0;
+    dp[2] = abs(a[1] - a[0]);
+    for(int i=3; i<=n; i++){
+        dp[i] = min(dp[i-1] + abs(a[i-1] - a[i-2]), dp[i-2] + abs(a[i-1] - a[i-3]));
     }
-    return res;
-}
-
-void test_case(){
-    cin >> n;
-    vector<int> a(n);
-    for(auto &x: a) cin >> x;
-
-    vector<int> dp(n);
-    dp[0] = 0;
-    dp[1] = abs(a[0] -a[1]);
-    for(int i = 2; i < n; i++){
-        dp[i] = min(dp[i-1] + abs(a[i] - a[i-1]), dp[i-2] + abs(a[i] - a[i-2]));
-    }
-    cout << dp[n-1] << endl;
+    debug()<<deb(dp);
+    cout<<dp[n]<<endl;
 }
 int32_t main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    test = 1;
+    cout.tie(0);
+    test=1;
     //cin>>test;
-    while(test--)
-        test_case();
-#ifndef ONLINE_JUDGE
-    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s." << endl;
-#endif
+    while(test--){
+        solve();
+    }
+    #ifndef ONLINE_JUDGE
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s." << endl;
+    #endif
     return 0;
 }
