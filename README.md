da470b5356b49b5e152441cbe104012dfbf85ff3
Manasvi Gaur <manasvigaur619@gmail.com>
2022-05-31 21:40:00 +0530

    Add solution for problem `E - Number of Groups` Link: https://codeforces.com/contest/1691/problem/E Tags: data structures, dfs and similar, dsu, graphs, greedy, sortings, *2300 Ref: https://codeforces.com/contest/1691/submission/159064473

--- README.md
+++ README.md
@@ -12,10 +12,11 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-7 | [G - Count the Trains](https://codeforces.com/contest/1690/problem/G) | [GNU C++20 (64)](./codeforces/1690/G.cpp) | `binary search` `data structures` `greedy` `sortings` `*2000` | Jun/07/2022 22:31 | 
-6 | [C - Line Empire](https://codeforces.com/contest/1659/problem/C) | [GNU C++20 (64)](./codeforces/1659/C.cpp) | `binary search` `brute force` `dp` `greedy` `implementation` `math` `*1500` | Jun/06/2022 23:57 | 
-5 | [A - Red Versus Blue](https://codeforces.com/contest/1659/problem/A) | [GNU C++20 (64)](./codeforces/1659/A.cpp) | `constructive algorithms` `greedy` `implementation` `math` `*1000` | Jun/06/2022 22:29 | 
-4 | [F1 - Promising String (easy version)](https://codeforces.com/contest/1660/problem/F1) | [GNU C++20 (64)](./codeforces/1660/F1.cpp) | `brute force` `implementation` `math` `strings` `*1700` | Jun/03/2022 17:43 | 
-3 | [E - Matrix and Shifts](https://codeforces.com/contest/1660/problem/E) | [GNU C++20 (64)](./codeforces/1660/E.cpp) | `brute force` `constructive algorithms` `greedy` `implementation` `*1600` | Jun/02/2022 23:53 | 
-2 | [D - Maximum Product Strikes Back](https://codeforces.com/contest/1660/problem/D) | [GNU C++20 (64)](./codeforces/1660/D.cpp) | `brute force` `implementation` `math` `two pointers` `*1600` | Jun/02/2022 23:19 | 
-1 | [C - Get an Even String](https://codeforces.com/contest/1660/problem/C) | [GNU C++20 (64)](./codeforces/1660/C.cpp) | `dp` `greedy` `strings` `*1300` | Jun/02/2022 22:41 | 
+8 | [G - Count the Trains](https://codeforces.com/contest/1690/problem/G) | [GNU C++20 (64)](./codeforces/1690/G.cpp) | `binary search` `data structures` `greedy` `sortings` `*2000` | Jun/07/2022 22:31 | 
+7 | [C - Line Empire](https://codeforces.com/contest/1659/problem/C) | [GNU C++20 (64)](./codeforces/1659/C.cpp) | `binary search` `brute force` `dp` `greedy` `implementation` `math` `*1500` | Jun/06/2022 23:57 | 
+6 | [A - Red Versus Blue](https://codeforces.com/contest/1659/problem/A) | [GNU C++20 (64)](./codeforces/1659/A.cpp) | `constructive algorithms` `greedy` `implementation` `math` `*1000` | Jun/06/2022 22:29 | 
+5 | [F1 - Promising String (easy version)](https://codeforces.com/contest/1660/problem/F1) | [GNU C++20 (64)](./codeforces/1660/F1.cpp) | `brute force` `implementation` `math` `strings` `*1700` | Jun/03/2022 17:43 | 
+4 | [E - Matrix and Shifts](https://codeforces.com/contest/1660/problem/E) | [GNU C++20 (64)](./codeforces/1660/E.cpp) | `brute force` `constructive algorithms` `greedy` `implementation` `*1600` | Jun/02/2022 23:53 | 
+3 | [D - Maximum Product Strikes Back](https://codeforces.com/contest/1660/problem/D) | [GNU C++20 (64)](./codeforces/1660/D.cpp) | `brute force` `implementation` `math` `two pointers` `*1600` | Jun/02/2022 23:19 | 
+2 | [C - Get an Even String](https://codeforces.com/contest/1660/problem/C) | [GNU C++20 (64)](./codeforces/1660/C.cpp) | `dp` `greedy` `strings` `*1300` | Jun/02/2022 22:41 | 
+1 | [E - Number of Groups](https://codeforces.com/contest/1691/problem/E) | [GNU C++20 (64)](./codeforces/1691/E.cpp) | `data structures` `dfs and similar` `dsu` `graphs` `greedy` `sortings` `*2300` | May/31/2022 21:40 | 
---
+++ codeforces/1691/E.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <climits>
+#include <iomanip>
+#include <tuple>
+#include <list>
+#include <bitset>
+#include <queue>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <chrono>
+#include <stack>
+#include <functional>
+#include <utility>
+#include <numeric>
+
+using namespace std;
+ 
+/******************************MACROS******************************/
+ 
+#define int long long
+#define ld long double
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define vt vector
+#define pb push_back
+#define F first
+#define S second
+#define u_s unordered_set
+#define u_m unordered_map
+#define p_q priority_queue
+#define all0(x) memset((x), 0, sizeof((x)))
+
+/*****************************DEBUGGING*TEMPLATE******************************/
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifndef ONLINE_JUDGE
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define deb(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+/******************************ALGORITHMS******************************/
+int __pow(int x, int y, int m) {int res = 1;while(y>0){if(y&1) res = (res*x)%m; y>>=1; x = (x*x)%m;}return res%m;}
+int __pow(int x, int y) {int res = 1;while(y>0){if(y&1) res = (res*x); y>>=1; x = (x*x);}return res;}
+int gcd(int a, int b) {if(b==0) return a; return gcd(b, a % b);}
+int mi(int x, int M) { return __pow(x,M-2, M);}
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
+
+/******************************main*code******************************/
+
+class disjointSet {
+    private:
+        vector<int> parent, rank, setSize;
+        //parent: path toward root of disjoint_set OR parent of every element in disjoint set
+        //rank: upper bound of the actual height of the tree; not reliable as actual measure
+        //setSize = size of each disjoint set
+        int numSets;
+    public:
+        disjointSet(int n){
+            setSize.assign(n, 1);
+            numSets = n;
+            for(int i=0; i<n; i++) parent.push_back(i);
+            rank.assign(n, 0);
+        }
+
+        int findParent(int node){
+            if(parent[node] != node){
+                parent[node] = findParent(parent[node]);
+            }
+            return parent[node];
+        }
+
+        void dsu(int i, int j){
+            int id_i = findParent(i);
+            int id_j = findParent(j);
+            if(id_i == id_j) return;
+            numSets--;
+            if(rank[id_i] < rank[id_j]){
+                parent[id_i] = id_j;
+                setSize[id_j] += setSize[id_i];
+            }
+            else{
+                parent[id_j] = id_i;
+                setSize[id_i] += setSize[id_j];
+                if(rank[id_i] == rank[id_j])
+                    rank[id_i]++;
+            }
+            return;
+        }
+
+        int countDisjointSets(){
+            return numSets;
+        }
+        int sizeOfSet(int node){
+            return setSize[findParent(node)];
+        }
+};
+
+const int N = 2e5+1, inf = 1e17, M = 1e9 + 7;
+int test, n, m, k, query;
+void solve(){
+    cin >> n;
+    vector<vector<int>> a;
+    for(int i =0; i< n; i++){
+        int c, l, r;
+        cin >> c >> l >> r;
+        a.push_back({l, r, c});
+    }
+    sort(all(a));
+    disjointSet ds(n);
+    set<vector<int>> red, blue; // y, x, ind
+    for(int i = 0; i< n ;i++){
+        while(!red.empty()){
+            vector<int> v = *red.begin();
+            if(v[0] < a[i][0]) red.erase(red.begin());
+            else break;
+        }
+
+        while(!blue.empty()){
+            vector<int> v = *blue.begin();
+            if(v[0] < a[i][0]) blue.erase(blue.begin());
+            else break;
+        }
+
+        if(a[i][2] == 0){
+            red.insert({a[i][1], a[i][0], i});
+            for(vector<int> v: blue){
+                ds.dsu(i, v[2]);
+            }
+            while(blue.size() > 1) blue.erase(blue.begin());
+        }else{
+            blue.insert({a[i][1], a[i][0], i});
+            for(vector<int> v: red){
+                ds.dsu(i, v[2]);
+            }
+            while(red.size() > 1) red.erase(red.begin());
+        }
+    }
+    
+    cout << ds.countDisjointSets() << endl;
+}
+
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    test = 1;
+    cin >> test;
+    for(int i = 1; i <= test; i++){
+        //cout << "Case #" << i << ": ";
+        solve();
+    }
+    // #ifndef ONLINE_JUDGE
+    //     cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s." << endl;
+    // #endif
+    return 0;
+}
